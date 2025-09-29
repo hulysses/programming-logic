aa42b7d7947b45590fcb7b780be67d56a9a8fa09
hulysses <hulyssesmaga@gmail.com>
2025-09-29 15:34:25 -0300

    add example outputs and primitive type demonstration in Main class

--- basic/src/Main.java
+++ basic/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
+
     // Entry point of the program
     public static void main(String[] args) {
+
+        // Class 01
         System.out.println("Hello World!");
+
+        // Class 02
+        // Primitive types
+        int age = 22;
+        System.out.println("Age: " + age);
+
+        // Class 03
     }
 }
