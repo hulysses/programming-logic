a6b26c6ce3b1b073b1342286155433c8d5f809a0
hulysses <hulyssesmaga@gmail.com>
2025-09-30 14:02:02 -0300

    add TaxCalculatorCondition class to demonstrate tax calculation based on salary

---
+++ basic/src/Exercises/TaxCalculatorCondition.java
@@ -0,0 +1,19 @@
+package Exercises;
+
+public class TaxCalculatorCondition {
+
+    public static void main(String[] args) {
+        float salary = 2000.50F;
+        float taxAmount;
+
+        if (salary > 3000) {
+            taxAmount = salary * 0.30F;
+        } else if (salary > 2000) {
+            taxAmount = salary * 0.15F;
+        } else {
+            taxAmount = salary * 0.05F;
+        }
+
+        System.out.println("Tax Amount: " + taxAmount);
+    }
+}
